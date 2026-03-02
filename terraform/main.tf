0d3caf19b3d5551e03d111918596a746bb0ab618
Ilyes Oueslati <69060633+IlyesOueslati@users.noreply.github.com>
2026-03-02 12:49:10 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,25 @@
+# -------------------------------------------------------
+# VPC Resource
+# -------------------------------------------------------
 
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "ecommerce-vpc"
+  }
+}
+
+# -------------------------------------------------------
+# Public Subnet
+# -------------------------------------------------------
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "ecommerce-public-subnet"
+  }
+}
