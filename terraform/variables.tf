d70a9905aa9b26345d5573a50fc1e812520a34b0
Ilyes Oueslati <69060633+IlyesOueslati@users.noreply.github.com>
2026-03-02 12:48:55 +0100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+# -------------------------------------------------------
+# Input Variables
+# -------------------------------------------------------
+
+variable "aws_region" {
+  description = "AWS region for deployment"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for main VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
