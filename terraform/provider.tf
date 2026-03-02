020f1fdb332e28f5429c941b7ae6004657e7526d
Ilyes Oueslati <69060633+IlyesOueslati@users.noreply.github.com>
2026-03-02 12:48:20 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,11 @@
 
+# -------------------------------------------------------
+# AWS Provider Configuration
+# -------------------------------------------------------
+# This defines the AWS region where infrastructure
+# resources will be provisioned.
+# -------------------------------------------------------
+
+provider "aws" {
+  region = var.aws_region
+}
